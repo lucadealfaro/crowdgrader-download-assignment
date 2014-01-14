c5c89920fb4f4314385eca52429bad38683ff6db
Luca de Alfaro <luca@dealfaro.org>
2014-01-14 11:01:46 -0800

    Ok, first working version.

--- download_assignment.py
+++ download_assignment.py
@@ -26,10 +26,16 @@ import getopt
 import json
 import os
 import os.path
+import re
+import urllib2
 import sys
 
 def usage():
     print "Usage: download_assignment.py <assignment_file.json> <destination_directory>"
+
+
+# Mapping from emails to (first_name, last_name) pairs.
+email_to_names = {}
 
 
 def main():
@@ -51,9 +57,7 @@ def main():
         sys.exit(1)
 
     assignment_data = read_json_file(json_file_name)
-    write_assignment_data(assignment_data)
-    download_submissions(assignment_data)
-    download_reviews(assignment_data)
+    write_assignment_data(dest_dir, assignment_data)
 
     print "All assignment data downloaded."
 
@@ -73,19 +77,115 @@ def read_json_file(json_file_name):
         sys.exit(2)
 
 
-def write_assignment_data(assignment_data):
+def make_directory(dir_name):
+    if not check_directory_exists(dir_name):
+        os.makedirs(dir_name)
+
+
+def write_assignment_data(dir_name, d):
     """Writes in human-readable form the main data of the assignment."""
-    pass
+    make_directory(dir_name)
+    f = open(os.path.join(dir_name, "info.txt"), 'w')
+    f.write("Assignment: %r\n" % d.get('Assignment'))
+    f.write("Number of submissions: %d\n" % len(d.get('Submission list', [])))
+    f.close()
+    # Generates a hash mapping from email to (first, last) names,
+    # useful to print better info on submissions.
+    for subm in d.get('Submission list', []):
+        email_to_names[subm.get('User email')] = (
+            subm.get('First name'), subm.get('Last name'))
+    for subm in d.get('Submission list', []):
+        download_submissions(dir_name, subm)
+        
+
+def download_submissions(dir_name, subm):
+    """Downloads a submission into its own directory."""
+    # First, creates the directory.
+    print "Downloading submission for:", subm.get('User email')
+    subm_dir_name = os.path.join(dir_name, subm.get('User email', 'unknown-user'))
+    make_directory(subm_dir_name)
+    f = open(os.path.join(subm_dir_name, "info.txt"), 'w')
+    f.write("Email: %r\n" % subm.get('User email'))
+    f.write("First name: %r\n" % subm.get('First name'))
+    f.write("Last name: %r\n" % subm.get('Last name'))
+    f.write("Submission date:%r\n" % subm.get('Submission date'))
+    subm_link = subm.get('Submitted link')
+    if subm_link is not None:
+        f.write("Submitted link: %r\n" % subm_link)
+    members = subm.get('Group members')
+    if members is not None:
+        for m in members:
+            (first_name, last_name) = email_to_names.get(m)
+            f.write("    Group member: %r : %r %r\n" % (m, first_name, last_name))
+    f.close()
+    # Downloads the submission itself.
+    text_url = subm.get('Submission')
+    download_text(subm_dir_name, text_url, name='submission.txt')
+    # Downloads any uploaded file.
+    upload_url = subm.get('Uploaded file')
+    download_file(subm_dir_name, upload_url, name='attachment.txt')
+    # Downloads any reviews.
+    download_reviews(subm_dir_name, subm)
 
 
-def download_submissions(assignment_data):
-    """Downloads all the submissions to the assignment, each into their own directory."""
-    pass
+def download_reviews(subm_dir_name, subm):
+    """Downlads the reviews for a given submission."""
+    print "Downloading reviews..."
+    w, _ = my_url_open(subm.get('Reviews'))
+    f = open(os.path.join(subm_dir_name, "reviews.txt"), 'w')
+    revs = json.loads(w)
+    for r in revs.get('Reviews'):
+        f.write("Review by: %r on: %r\n" % (r.get('Review author'), 
+                                            r.get('Completed date')))
+        f.write("Declined: %s\n" % str(r.get('Declined')))
+        f.write("Grade: %r\n" % r.get('Review grade'))
+        f.write("Helpfulness: %r\n\n" % r.get('Review helpfulness'))
+        f.write("Review:\n\n")
+        f.write("%r\n\n" % r.get('Reviewer comments'))
+        if r.get('Review feedback') is not None:
+            f.write("\nFeedback:\n\n")
+            f.write("%r\n\n" % r.get('Review feedback'))
+        f.write("==============================\n\n")
+    f.close()
+    
+
+def download_text(dir_name, url, name='submission.txt'):
+    w, _ = my_url_open(url)
+    sub = json.loads(w)
+    f = open(os.path.join(dir_name, name), 'w')
+    f.write('%r' % sub.get('Submission'))
+    f.close()
 
 
-def download_reviews(assignment_data):
-    """Downlads the reviews that each of the submissions received."""
-    pass
+def download_file(dir_name, url, name='attachment.txt'):
+    w, h = my_url_open(url)
+    filename = get_original_filename(h, name=name)
+    print "Downloading:", filename
+    f = open(os.path.join(dir_name, filename), 'w')
+    f.write(w)
+    f.close()
+    
+
+def get_original_filename(h, name="attachment.txt"):
+    for el in h:
+        if el.startswith('Content-Disposition:'):
+            filename = re.findall("filename=(\S+)", el)            
+            if filename is not None and len(filename) > 0:
+                return filename[0][1:-1]
+    return name
+
+
+def my_url_open(url, n_retries=3):
+    request = urllib2.Request(url)
+    for i in range(n_retries):
+        try:
+            w = urllib2.urlopen(request)
+            return w.read(), w.info().headers
+        except Exception, e:
+            print "Error in download:", e
+        print "Retrying"
+    print "Failed"
+    return '', []
 
 
 if __name__ == '__main__':
